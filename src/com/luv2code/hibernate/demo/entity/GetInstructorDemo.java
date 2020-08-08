8e51761b45a8de9bc7e6997a9367a6016b02ac14
kaarthikgurram <66755858+kaarthikgurram@users.noreply.github.com>
2020-08-08 14:33:25 -0600

    initial commit

---
+++ src/com/luv2code/hibernate/demo/entity/GetInstructorDemo.java
@@ -0,0 +1,44 @@
+package com.luv2code.hibernate.demo.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GetInstructorDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		SessionFactory factory = new Configuration()
+									.configure("hibernate.cfg.xml")
+									.addAnnotatedClass(Instructor.class)
+									.addAnnotatedClass(InstructorDetail.class)
+									.buildSessionFactory();
+		
+		Session session = factory.getCurrentSession();
+		
+		try{
+			System.out.println("process started");
+			
+			session.beginTransaction();
+			
+			int id=2;
+			
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, id);
+			
+			System.out.println(instructorDetail);
+			System.out.println(instructorDetail.getInstructor());
+			
+			session.getTransaction().commit();
+			
+			System.out.println("Process finished successfully");
+			
+		} catch(Exception exc) {
+			exc.printStackTrace();
+		}
+		finally {
+			session.close();
+			factory.close();
+		}
+		}
+}
